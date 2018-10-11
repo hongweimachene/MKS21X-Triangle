4fd93231ee668aa4551071ad14f9be29856a399f
hongweimachene <hchen00@stuy.edu>
2018-10-11 10:04:35 -0400

    Point class

--- Point.java
+++ Point.java
@@ -23,13 +23,13 @@ public class Point{
     return new Point(this);
   }
   public double distanceTo(Point p2) {
-    return Math.sqrt(Math.pow(p2.getX() - this.getX(), 2) + Math.pow(p2.getY() - this.getY(), 2));
+    return Math.sqrt(Math.pow(p2.x - x, 2) + Math.pow(p2.y - y, 2));
   }
-  public double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
   }
   public String toString() {
-    return "Point(" + x + "," + y + ")";
+    return "(" + x + "," + y + ")";
   }
 
 }
